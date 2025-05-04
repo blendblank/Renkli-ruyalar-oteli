2ce543aa9f75669a39121fbb0eca5a46ee4f8cdc
blendblank <ozkanfidan24@gmail.com>
2025-05-04 18:36:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function showSurprise() {
+    const popup = document.getElementById("memePopup");
+    const audio = document.getElementById("memeAudio");
+    popup.style.display = "block";
+    audio.play();
+}
